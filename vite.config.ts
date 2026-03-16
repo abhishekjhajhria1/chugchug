ae29bfe18e70f6ceb1c3dfd682e722362d0ce0da
abhishek jhajhria <jhajhria2112@gmail.com>
2026-03-16 23:41:16 +0530

    erre

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,29 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('scheduler')) {
+              return 'vendor-react'
+            }
+            if (id.includes('@supabase')) {
+              return 'vendor-supabase'
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor-lucide'
+            }
+            if (id.includes('react-router') || id.includes('react-router-dom') || id.includes('@remix-run')) {
+              return 'vendor-router'
+            }
+            // all other node_modules
+            return 'vendor-core'
+          }
+        }
+      }
+    }
+  }
 })
